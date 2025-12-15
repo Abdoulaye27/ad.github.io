a042f10883e908a7e897b42bffb40a8ebaa368e7
Abdoulaye27 <am.diallo.2001@gmail.com>
2025-12-14 20:37:44 -0800

    route fixing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,35 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from '@/App.jsx'
+// import '@/index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//   <App />
+//   // </React.StrictMode>,
+// )
+
+// if (import.meta.hot) {
+//   import.meta.hot.on('vite:beforeUpdate', () => {
+//     window.parent?.postMessage({ type: 'sandbox:beforeUpdate' }, '*');
+//   });
+//   import.meta.hot.on('vite:afterUpdate', () => {
+//     window.parent?.postMessage({ type: 'sandbox:afterUpdate' }, '*');
+//   });
+// }
+
+
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>,
+  <HashRouter>
+    <App />
+  </HashRouter>
 )
 
 if (import.meta.hot) {
@@ -17,6 +40,3 @@ if (import.meta.hot) {
     window.parent?.postMessage({ type: 'sandbox:afterUpdate' }, '*');
   });
 }
-
-
-
